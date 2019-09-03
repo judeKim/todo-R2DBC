e591139aca484514acdaf1ad691392d491d6912e
digimon <digimon.1740@gmail.com>
2019-09-03 19:53:27 +0900

    핸들러 리팩토링

--- src/main/kotlin/com/digimon/demo/handler/TodoHandler.kt
+++ src/main/kotlin/com/digimon/demo/handler/TodoHandler.kt
@@ -3,8 +3,8 @@ package com.digimon.demo.handler
 import com.digimon.demo.domain.todo.Todo
 import com.digimon.demo.domain.todo.TodoRepository
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.*
@@ -12,6 +12,7 @@ import reactor.core.publisher.Mono
 import java.net.URI
 import java.time.LocalDateTime
 import java.util.*
+import java.util.stream.Collectors.toList
 
 @Component
 class TodoHandler {
@@ -21,15 +22,15 @@ class TodoHandler {
         this.repo = repo
     }
 
-    fun getAll(req: ServerRequest): Mono<ServerResponse> = ok()
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(repo.findAll(), Todo::class.java)
-            .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
+    fun getAll(req: ServerRequest): Mono<ServerResponse> =
+            repo.findAll().filter(Objects::nonNull)
+                    .collect(toList())
+                    .flatMap { ok().body(fromObject(it)) }
 
-    fun getById(req: ServerRequest): Mono<ServerResponse> = ok()
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(repo.findById(req.pathVariable("id").toLong()), Todo::class.java)
-            .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
+    fun getById(req: ServerRequest): Mono<ServerResponse> =
+            repo.findById(req.pathVariable("id").toLong())
+                    .flatMap { ok().body(fromObject(it)) }
+                    .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
 
     fun save(req: ServerRequest): Mono<ServerResponse> =
             repo.saveAll(req.bodyToMono(Todo::class.java))
